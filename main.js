2da904a977990eb3eb865398c6a8e5522203679e
shubhamdesale2003 <shubhamhanumantpatil@gmail.com>
2024-03-10 09:35:03 +0530

    all Set

---
+++ main.js
@@ -0,0 +1,43 @@
+
+const ai = document.querySelector(".ai");
+const java = document.querySelector(".java");
+const ds = document.querySelector(".ds");
+
+
+ai.addEventListener("click",()=>{
+    document.querySelector(".ai-content").style.display="block";
+    document.querySelector(".java-content").style.display="none";
+    document.querySelector(".ds-content").style.display="none";
+    //document.querySelector(".ai").className.add('.active');
+    ai.style.backgroundColor='white';
+    ai.style.color='black';
+    java.style.color='white';
+    ds.style.color='white';
+    ds.style.backgroundColor='transparent';
+    java.style.backgroundColor='transparent';
+
+})
+
+java.addEventListener("click",()=>{
+    document.querySelector(".java-content").style.display="block";
+    document.querySelector(".ai-content").style.display="none";
+    document.querySelector(".ds-content").style.display="none";
+    java.style.backgroundColor='white';
+    java.style.color='black';
+    ai.style.color='white';
+    ds.style.color='white';
+    ai.style.backgroundColor='transparent';
+    ds.style.backgroundColor='transparent';
+})
+
+ds.addEventListener("click",()=>{
+    document.querySelector(".ds-content").style.display="block";
+    document.querySelector(".ai-content").style.display="none";
+    document.querySelector(".java-content").style.display="none";
+    ds.style.backgroundColor='white';
+    ds.style.color='black';
+    java.style.color='white';
+    ai.style.color='white';
+    ai.style.backgroundColor='transparent';
+    java.style.backgroundColor='transparent';
+})
